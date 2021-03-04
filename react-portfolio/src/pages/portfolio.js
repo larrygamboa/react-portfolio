6083c4b11083966508c2ff29aaeabe2d56c9f4c3
Larry Gamboa <techmedia.lg@gmail.com>
2021-03-03 21:44:51 -0800

    create portfolio page

---
+++ react-portfolio/src/pages/portfolio.js
@@ -0,0 +1,101 @@
+import React from "react";
+import "../App.css";
+
+const portfolio = () => {
+    return (
+        <div id="">
+            <div className="container bg-white">
+                <div className="row">
+                    <div className="col-lg-9 col-xlg-9 p-4">
+                        <h2>Portfolio</h2>
+                        <hr />
+                        <p>Here are some examples of my current and past work. I've added some applications developed in the Full Stack Web Development program with UC San Diego Extension. I've also designed and built numerous websites throughout my 10+ year career which include corporate, medical, law and small business. You can preview more of my design work on <a href="https://www.behance.net/larrygamboa" target="_blank">Behance</a>.</p>
+                        <div className="row">
+                            <div className="col-6 col-sm-6 col-md-6 col-lg-6 col-xlg-6 p-3 text-center">
+                                <img src="../images/portfolio-express-handlebars-thumb.jpg" alt=" " className="img-fluid rounded mx-auto d-block" />
+                                <h5 className="mt-3 mb-3"><strong>Node Express Handlebars</strong></h5>
+                                <p><a href="https://salty-oasis-54918.herokuapp.com/" target="_blank">Preview &rarr;</a></p>
+                                <p><a href="https://github.com/larrygamboa/node-express-handlebars" target="_blank">GitHub Repo &rarr;</a></p>
+                            </div>
+                            <div className="col-6 col-sm-6 col-md-6 col-lg-6 col-xlg-6 p-3 text-center">
+                                <img src="../images/portfolio-employee-tracker-thumb.jpg" alt=" " className="img-fluid rounded mx-auto d-block" />
+                                <h5 className="mt-3 mb-3"><strong>Employee Tracker</strong></h5>
+                                <p><a href="https://drive.google.com/file/d/1GajnjWgNkObedsbb7IKGsOCM-BA1pvw-/view" target="_blank">Preview &rarr;</a></p>
+                                <p><a href="https://github.com/larrygamboa/employee-tracker" target="_blank">GitHub Repo &rarr;</a></p>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col-6 col-sm-6 col-md-6 col-lg-6 col-xlg-6 p-3 text-center">
+                                <img src="../images/portfolio-note-taker-thumb.jpg" alt=" " className="img-fluid rounded mx-auto d-block" />
+                                <h5 className="mt-3 mb-3"><strong>Note Taker</strong></h5>
+                                <p><a href="https://boiling-sands-71851.herokuapp.com/" target="_blank">Preview &rarr;</a></p>
+                                <p><a href="https://github.com/larrygamboa/note-taker" target="_blank">GitHub Repo &rarr;</a></p>
+                            </div>
+                            <div className="col-6 col-sm-6 col-md-6 col-lg-6 col-xlg-6 p-3 text-center">
+                                <img src="assets/images/portfolio-employee-tracker-thumb.jpg" alt=" " className="img-fluid rounded mx-auto d-block" />
+                                <h5 className="mt-3 mb-3"><strong>Team Generator</strong></h5>
+                                <p><a href="https://drive.google.com/file/d/1LJeSG-eqHl89O_qFHLQmPsA2dDfGSY9S/view" target="_blank">Preview &rarr;</a></p>
+                                <p><a href="https://github.com/larrygamboa/team-generator" target="_blank">GitHub Repo &rarr;</a></p>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col-6 col-sm-6 col-md-6 col-lg-6 col-xlg-6 p-3 text-center">
+                                <img src="../images/portfolio-readme-generator-thumb.jpg" alt=" " className="img-fluid rounded mx-auto d-block" />
+                                <h5 className="mt-3 mb-3"><strong>ReadMe Generator</strong></h5>
+                                <p><a href="https://drive.google.com/file/d/1MXMG7GiE4UewcFGNqAWiU05Gq3eioKAq/view" target="_blank">Preview &rarr;</a></p>
+                                <p><a href="https://github.com/larrygamboa/professional-readme-generator" target="_blank">GitHub Repo &rarr;</a></p>
+                            </div>
+                            <div className="col-6 col-sm-6 col-md-6 col-lg-6 col-xlg-6 p-3 text-center">
+                                <img src="../images/portfolio-day-planner-thumb.jpg" alt=" " className="img-fluid rounded mx-auto d-block" />
+                                <h5 className="mt-3 mb-3"><strong>Day Planner</strong></h5>
+                                <p><a href="https://larrygamboa.github.io/homework-5-day-planner/" target="_blank">Preview &rarr;</a></p>
+                                <p><a href="https://github.com/larrygamboa/homework-5-day-planner" target="_blank">GitHub Repo &rarr;</a></p>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col-6 col-sm-6 col-md-6 col-lg-6 col-xlg-6 p-3 text-center">
+                                <img src="../images/portfolio-caniglia-thumb.jpg" alt=" " className="img-fluid rounded mx-auto d-block" />
+                                <h5 className="mt-3 mb-3"><strong>Client website redesign</strong></h5>
+                                <p><a href="https://www.behance.net/gallery/100182867/Caniglia-Facial-Plastic-Surgery-homepage-design" target="_blank">Preview &rarr;</a></p>
+                            </div>
+                            <div className="col-6 col-sm-6 col-md-6 col-lg-6 col-xlg-6 p-3 text-center">
+                                <img src="../images/portfolio-pacific-thumb.jpg" alt=" " className="img-fluid rounded mx-auto d-block" />
+                                <h5 className="mt-3 mb-3"><strong>Client website redesign</strong></h5>
+                                <p><a href="https://www.behance.net/gallery/100182757/Pacific-Plastic-Surgery-homepage-design" target="_blank">Preview &rarr;</a></p>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col-6 col-sm-6 col-md-6 col-lg-6 col-xlg-6 p-3 text-center">
+                                <img src="../images/portfolio-petco-thumb.jpg" alt=" " className="img-fluid rounded mx-auto d-block" />
+                                <h5 className="mt-3 mb-3"><strong>Petco Summer Homepage</strong></h5>
+                                <p><a href="https://www.behance.net/gallery/103279859/Summer-event-homepage" target="_blank">Preview &rarr;</a></p>
+                            </div>
+                            <div className="col-6 col-sm-6 col-md-6 col-lg-6 col-xlg-6 p-3 text-center">
+                                <img src="../images/portfolio-nutrition-thumb.jpg" alt=" " className="img-fluid rounded mx-auto d-block" />
+                                <h5 className="mt-3 mb-3"><strong>Petco Nutrition Landing Page</strong></h5>
+                                <p><a href="https://www.behance.net/gallery/99965021/landing-page" target="_blank">Preview &rarr;</a></p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-lg-3 col-xlg-3 p-4">
+                        <h2>Let's Connect</h2>
+                        <hr />
+                        <ul className="connect">
+                            <li><a href="https://www.linkedin.com/in/gamboalarry/" target="_blank">LinkedIn &rarr;</a></li>
+                            <li><a href="https://github.com/larrygamboa/" target="_blank">GitHub &rarr;</a></li>
+                            <li><a href="https://www.behance.net/larrygamboa" target="_blank">Behance &rarr;</a></li>
+                            <li><a href="https://www.instagram.com/capturesbylg/" target="_blank">Instagram &rarr;</a></li>
+                            <li><hr /></li>
+                            <li><a href="mailto:techmedia.lg@gmail.com">techmedia.lg@gmail.com</a></li>
+                            <li>858.275.3161</li>
+                            <li><hr /></li>
+                            <li><a href="../images/larry_gamboa_2021resume_updated012021.pdf" target="_blank"><button type="button" className="btn btn-primary mb-3">Download Resum&eacute;</button></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default portfolio;
