cc279eb1de40fd6635010c848b9709700e0120b2
Larry Gamboa <techmedia.lg@gmail.com>
2021-03-05 15:49:11 -0800

    fix footer links

--- react-portfolio/src/components/Footer/index.js
+++ react-portfolio/src/components/Footer/index.js
@@ -7,7 +7,7 @@ const footer = () => {
             <div className="container">
                 <div className="row">
                     <div className="col text-center">
-                        <p className="mb-4"><Link to="/About">Introduction</Link> | <Link to="/Portfolio">Portfolio</Link> | <Link to="/Contact">Contact</Link></p>
+                        <p className="mb-4"><Link to="/Intro">Introduction</Link> | <Link to="/Portfolio">Portfolio</Link> | <Link to="/Contact">Contact</Link></p>
                         <p className="create"><strong>Created with <a href="https://getbootstrap.com/" target="_blank" rel="noreferrer">Bootstrap</a> and <a href="https://reactjs.org/" target="_blank" rel="noreferrer">React</a> by <a href="https://github.com/larrygamboa" target="_blank" rel="noreferrer">Larry Gamboa</a></strong></p>
                         <p><small>&copy; 2021 Larry Gamboa</small></p>
                     </div>
